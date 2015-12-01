fde8342cdd755477eaac0ea2c3be65b5c8bd4a73
Stephen Delaney <swdelaney2@gmail.com>
2015-12-01 16:24:49 -0600

    working with a create form

--- controllers/recommendations.rb
+++ controllers/recommendations.rb
@@ -6,9 +6,35 @@ get '/' do
 end
 
 get '/api' do
-# ClassRecs.all.to_json
+ClassRecs.all.to_json
 # ClassRecs.find_by(id: '1').to_json
-ClassRecs.where(contributor: 'James T').to_json
+# ClassRecs.where(contributor: 'James T').to_json
+end
+
+get '/api/:id' do
+  p params
+return ClassRecs.find(params[:id]).to_json
+end
+
+post '/create' do
+p params
+@newreccos = ClassRecs.new
+@newreccos.contributor = params[:contributor]
+@newreccos.place = params[:place]
+@newreccos.notes = params[:notes]
+@newreccos.typeofplace = params[:typeofplace]
+@newreccos.save
+@recs = ClassRecs.all
+erb :index
+end
+
+get '/create' do
+  erb :create_recommendation
+end
+
+get '/read' do
+  @recs = ClassRecs.all
+  erb :read_recommendations
 end
 
 end
